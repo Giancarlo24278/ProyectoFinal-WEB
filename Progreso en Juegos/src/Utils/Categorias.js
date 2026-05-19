f5d90984a4ab85a99803385475c2448605aa09e4
Giancarlo24278 <sag24278@uvg.edu.gt>
2026-05-19 16:05:59 -0600

    agrgando categorias osea ID y removiendo cosas inecesarias

---
+++ Progreso en Juegos/src/Utils/Categorias.js
@@ -0,0 +1,27 @@
+export const categorias = [
+  {
+    id: 'rpg',
+    nombre: 'RPG',
+    emoji: '⚔️'
+  },
+  {
+    id: 'rouguelike',
+    nombre: 'Rouguelike',
+    emoji: '🎲'
+  },
+  {
+    id: 'miedo',
+    nombre: 'Miedo',
+    emoji: '👻'
+  },
+  {
+    id: 'sandbox',
+    nombre: 'Sandbox',
+    emoji: '🧱'
+  },
+  {
+    id: 'multiplayer',
+    nombre: 'Multiplayer',
+    emoji: '👥'
+  }
+]
