1cc1a0c9743b615650b18c63aff6f638691afcce
Giancarlo24278 <sag24278@uvg.edu.gt>
2026-05-19 19:30:36 -0600

    agrgando index para seridor principal

---
+++ Progreso en Juegos/backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const itemsRoutes = require('./routes/items')
+
+const app = express()
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
+
+app.use(express.json())
+
+app.use('/api/items', itemsRoutes)
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`)
+})
