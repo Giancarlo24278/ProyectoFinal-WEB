d04eb802f209e7deda5bc129bf9740123124efa9
Giancarlo24278 <sag24278@uvg.edu.gt>
2026-05-26 15:47:34 -0600

    Actualizando main para soportar nuevo storage

--- Progreso en Juegos/frontend/src/main.jsx
+++ Progreso en Juegos/frontend/src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+import './index.css'
+
+import { StorageProvider } from './context/StorageProvider'
+import { ThemeProvider } from './context/ThemeProvider'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <StorageProvider>
+        <App />
+      </StorageProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
