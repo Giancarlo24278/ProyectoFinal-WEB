11c84c886f7c706df2e19b543a0b2f3846c7aa76
Giancarlo24278 <sag24278@uvg.edu.gt>
2026-05-26 18:48:32 -0600

    Mejorando Ui y optimizando un poco

--- Progreso en Juegos/frontend/src/Components/ItemCard.jsx
+++ Progreso en Juegos/frontend/src/Components/ItemCard.jsx
@@ -1,17 +1,32 @@
 function Carta({ item }) {
   return (
-    <div>
-      <h2>{item.nombre}</h2>
-
-      <p>
-        {item.atributos.progreso}%
-      </p>
-
+    <div className="card">
       <img
         src={item.atributos.imagen}
         alt={item.nombre}
-        width="200"
       />
+
+      <div className="card-content">
+        <h2>{item.nombre}</h2>
+
+        <p>
+          {item.atributos.horasJugadas} /
+          {item.atributos.horasTotales} horas
+        </p>
+
+        <div className="barra">
+          <div
+            className="progreso"
+            style={{
+              width: `${item.atributos.progreso}%`
+            }}
+          ></div>
+        </div>
+
+        <p>
+          {item.atributos.progreso}% completado
+        </p>
+      </div>
     </div>
   )
 }
