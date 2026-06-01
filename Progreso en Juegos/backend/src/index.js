9f339e3f628a6ca3d24d712d88d3394fa50808c6
Giancarlo24278 <sag24278@uvg.edu.gt>
2026-05-31 21:50:21 -0600

    Actualizando datos de Backend

--- Progreso en Juegos/backend/src/index.js
+++ Progreso en Juegos/backend/src/index.js
@@ -6,14 +6,14 @@ const itemsRoutes = require('./routes/items')
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: process.env.FRONTEND_URL
 }))
 
 app.use(express.json())
 
 app.use('/api/items', itemsRoutes)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`)
